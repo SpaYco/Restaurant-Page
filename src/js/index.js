2562a6bdd8ad0c58835d6348ed2a8c0c87423f51
SpaYco <zizou.aziz101@gmail.com>
2020-07-28 14:06:15 +0100

    updated webpack

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import "../css/main.scss";
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min';
+console.log("Hello webpack!")
+const fancyFunc = () => {
+    return [1, 2];
+  };
+  
+  const [a, b] = fancyFunc();
